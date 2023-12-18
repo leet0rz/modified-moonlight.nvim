a79d9e709891e362312ae663f1abd8cc783582cb
leet0rz <eskemoporno@hotmail.com>
2023-12-18 11:01:14 +0100

    fixes

--- lua/moonlight/after.lua
+++ lua/moonlight/after.lua
@@ -1,2 +1,12 @@
+-- fix statusline
 vim.cmd('hi StatusLine guibg=NONE')
 vim.cmd('hi StatusLineNC guibg=NONE')
+
+-- fix neovim nightly errors:
+
+-- first
+-- vim.cmd("highlight @variable.lua guifg=#38d5d5")
+-- vim.cmd('highlight Identifier guifg=#38d5d5')
+
+vim.cmd('highlight @variable.lua guifg=#38d5d5')
+vim.cmd('highlight Identifier guifg=#2aa2a2')
